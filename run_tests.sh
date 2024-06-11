eb81c66c1eab3a3fc7a100345f23dfef851c6286
Grische <github@grische.xyz>
2024-06-11 15:18:59 +0200

    cleanup and improve run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -eEu
+set -o pipefail
+
 pushd tests > /dev/null
 passed=0
 failed=0
 for f in t*.sh; do
-    echo -n "$f "
-    python3 ../minifier.py $f > _tmp_$f
-    if [ `diff _tmp_$f minified_$f | wc -c` -ne 0 ]; then
+    echo -n "${f} "
+    python3 ../minifier.py "${f}" > "_tmp_${f}"
+    diffsize=$(diff "_tmp_${f}" "minified_${f}" | wc -c)
+    if [[ ${diffsize} -ne 0 ]]; then
         failed=$((failed+1))
         echo FAILED
     else
@@ -15,5 +19,5 @@ for f in t*.sh; do
     fi
     rm _tmp_$f
 done
-echo PASSED $passed, FAILED $failed
+echo "PASSED ${passed}, FAILED ${failed}"
 popd > /dev/null
